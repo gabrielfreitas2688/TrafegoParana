f84f3abc2fca33329db8fab6c7cc6b420d376bb3
Gabriel Freitas <gabrielfreitas2688@gmail.com>
2023-05-11 18:20:32 -0300

    Adicionado a biblioteca TASSREACT junto ao projeto e realizado o primeiro teste com êxito

--- exemplo.py
+++ exemplo.py
@@ -1,6 +1,8 @@
 ##Importantdo as bibliotecas
 import cv2
+import pytesseract
 
+#Processamento inicial da imagem 
 def RoiPlaca(source):
     ##Carrega a imagem
     imagem = cv2.imread("assets/exemplos/carro.jpg")
@@ -8,10 +10,10 @@ def RoiPlaca(source):
 
     ##Altera a imagem para escala cinza pois é possível apenas encontrar padrões utilizando preto e branco
     imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-
+   
     ##Converte todas as cores para preto e banco (binariza a imagem)
     _, bin = cv2.threshold(imagemcinza, 90, 255, cv2.THRESH_BINARY)
-
+  
     ##Realzia o desfoque da imagem afim de melhorar a nitidez e encontrar os melhores contornos 
     desfoque = cv2.GaussianBlur(bin, (5,5), 0)
 
@@ -19,8 +21,8 @@ def RoiPlaca(source):
     contornos, hier = cv2.findContours(desfoque, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     ##Desenha os contornos na imagem principal(imagem, contornos, ID do contorno "-1" pega todos, cor, espeçura)
-    #cv2.drawContours(imagem, contornos, -1, (0, 255, 0), 2)
-    #cv2.imshow("contornos", imagem)
+    cv2.drawContours(imagem, contornos, -1, (0, 255, 0), 2)
+   # cv2.imshow("contornos", imagem)
 
     for c in contornos:
         ##Percorre os contornos fechados
@@ -51,16 +53,41 @@ def preProcessamentoRoi():
     #Se não tiver placa ele não retorna nada
     if img_roi is None:
         return
+    
+    #Aumenta o tamanho da imagem para o tesseract não ter problema em ler 
+    img_tamanho = cv2.resize(img_roi, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC)
 
-    cv2.imshow("roi", img_roi)
+    ##Altera a imagem para escala cinza 
+    img_roi_cinza = cv2.cvtColor(img_tamanho, cv2.COLOR_BGR2GRAY)
+
+    ##Deixa a imagem binarizara (preto e branco)
+    _, img_roi_bin = cv2.threshold(img_roi_cinza, 52, 255, cv2.THRESH_BINARY)
+
+    cv2.imwrite("assets/cortes/roi-bin.jpg", img_roi_bin)
+
+    cv2.imshow("roi", img_roi_bin)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+#Passando o tesseract na imagem 
+def ocrImagemRoiPlaca():
+    #Pega a imagem
+    img_roi = cv2.imread("assets/cortes/roi-img.jpg")
+
+    #configuração para ler apenas letras maiúsculas e numeros 
+    config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 6'
+
+    #Saída (usado a biblitoca ingles pois PTBR tem erros)
+    saida  = pytesseract.image_to_string(img_roi, lang="eng", config=config)
+
+    print(saida)
 
 if __name__ == "__main__":
 
     source = "assets/exemplos/carro.jpg";
     
-    #RoiPlaca(source)
-    preProcessamentoRoi()
+    RoiPlaca(source)
+    #preProcessamentoRoi()
+    #ocrImagemRoiPlaca()
  
